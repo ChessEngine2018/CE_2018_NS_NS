7fa2d96c0bd30e76bd2ffbdcf0e787915aac32b2
Neil <Neil@DESKTOP-UV2UT9E>
2018-04-03 16:10:40 -0400

    Removed everything.

--- src/ChessEngine/board/Board.java
+++ src/ChessEngine/board/Board.java
@@ -1,42 +1,8 @@
 package ChessEngine.board;
 
 public class Board {
-	/** Instance of chess board */
-	private static Board instance;
-	/** Chess board grid */
-	private static Location[][] grid;
-	/** Number of black pawns */
-	private static int numBPawns;
-	/** Number of black Rooks */
-	private static int numBRooks;
-	/** Number of black bishops */
-	private static int numBBishops;
-	/** Number of black knights */
-	private static int numBKnights;
 	
+	private static Board board;
 
-	/**
-	 * Returns instance of chess board, if it is null a new instance is created
-	 * 
-	 * @return instance of chess board
-	 */
-	public Board getInstance() {
-		if (instance == null) {
-			instance = new Board();
-		}
-		return instance;
-	}
-
-	/**
-	 * Creates a new chess board with empty locations
-	 */
-	private Board() {
-		Location[][] grid = new Location[8][8];
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				grid[i][j] = new Location(i, j);
-			}
-		}
-		this.grid = grid;
-	}
+	
 }
