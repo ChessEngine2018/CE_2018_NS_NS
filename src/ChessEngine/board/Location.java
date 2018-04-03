a372c2c3e7ef989434f3b97eef155bcb430bbefb
Nathan Seamon <nseamon@ncsu.edu>
2018-04-03 15:38:25 -0400

    Intial stuff

---
+++ src/ChessEngine/board/Location.java
@@ -0,0 +1,15 @@
+package ChessEngine.board;
+
+/**
+ * Class contains location objects, which have information about a specific
+ * square on the board
+ * 
+ * @author Nathan Seamon
+ *
+ */
+public class Location {
+
+	public Location(int row, int col) {
+		
+	}
+}
