334d1c8c07ba44fef1307304a0c9493c9e4e5151
Nathan Seamon <nseamon@ncsu.edu>
2018-04-03 16:32:54 -0400

    Piece

--- src/ChessEngine/model/piece/Piece.java
+++ src/ChessEngine/model/piece/Piece.java
@@ -1,5 +1,16 @@
 package ChessEngine.model.piece;
 
-public abstract class Piece {
+import ChessEngine.model.board.Location;
 
+public abstract class Piece {
+	/**Point value*/
+	private int pointValue;
+	/**Location of piece*/
+	private Location location;
+	/** Piece team */
+	public static enum Team {BLACK, WHITE};
+	
+	public Piece(int pointValue, Location location) {
+		
+	}
 }
