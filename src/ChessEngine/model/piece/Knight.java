a13e0e539ef97a1b20f5a557add2642696421eb9
Nathan Seamon <nseamon@ncsu.edu>
2018-04-07 01:07:17 -0400

    Knight move method

--- src/ChessEngine/model/piece/Knight.java
+++ src/ChessEngine/model/piece/Knight.java
@@ -10,13 +10,13 @@ import ChessEngine.model.gameplay.Move;
 import ChessEngine.model.piece.Piece.Team;
 
 public class Knight extends Piece {
-	
+
 	private static final int POINT_VAL = 3;
-	
+
 	public Knight(Piece.Team team) {
 		super(Piece.Type.KNIGHT, team, POINT_VAL);
 	}
-	
+
 	/**
 	 * Method returns a list of all the valid moves for the piece
 	 * 
@@ -33,7 +33,40 @@ public class Knight extends Piece {
 		Board board = Board.getBoard();
 		Grid grid = board.getGrid();
 
-		return null;
+		
+		if(((row + 2) < (8)) && ((col + 1) < 8)) {
+			moves.add(new Move(this.getTeam(), loc, grid.getLocation((row + 2), (col + 1))));
+		}
+		
+		if(((row + 1) < (8)) && ((col + 2) < 8)) {
+			moves.add(new Move(this.getTeam(), loc, grid.getLocation((row + 1), (col + 2))));
+		}
+		
+
+		if(((row + 2) < (8)) && ((col - 1) >= 0)) {
+			moves.add(new Move(this.getTeam(), loc, grid.getLocation((row + 2), (col - 1))));
+		}
+		
+		if(((row + 1) < 8) && ((col - 2) >= 0)) {
+			moves.add(new Move(this.getTeam(), loc, grid.getLocation((row + 1), (col - 2))));
+		}
+		
+		if(((row - 2) >= 0) && ((col - 1) >= 0)) {
+			moves.add(new Move(this.getTeam(), loc, grid.getLocation((row - 2), (col - 1))));
+		}
+		
+		if(((row - 1) >= 0) && ((col - 2) >= 0)) {
+			moves.add(new Move(this.getTeam(), loc, grid.getLocation((row - 1), (col - 2))));
+		}
+		if(((row - 2) >= 0) && ((col + 1) < 8)) {
+			moves.add(new Move(this.getTeam(), loc, grid.getLocation((row - 2), (col + 1))));
+		}
+		
+		if(((row - 1) >= 0) && ((col + 2) < 8)) {
+			moves.add(new Move(this.getTeam(), loc, grid.getLocation((row - 1), (col + 2))));
+		}
+		
+		return moves;
 	}
 
 }
