da6de70c162a86f71dcaa5b0dbcaf61c473ecd90
Nathan Seamon <nseamon@ncsu.edu>
2018-04-08 22:37:13 -0400

    Removed unnessisary syntax I added

--- src/ChessEngine/model/board/Board.java
+++ src/ChessEngine/model/board/Board.java
@@ -68,30 +68,30 @@ public class Board {
 	 */
 	public void standardBoard() {
 		reset();
-		add(0, 0, new Rook(Piece.Team.BLACK, grid.getLocation(0, 0)));
-		add(0, 1, new Knight(Piece.Team.BLACK, grid.getLocation(0, 1)));
-		add(0, 2, new Bishop(Piece.Team.BLACK, grid.getLocation(0, 2)));
-		add(0, 3, new Queen(Piece.Team.BLACK, grid.getLocation(0, 3)));
-		add(0, 4, new King(Piece.Team.BLACK, grid.getLocation(0, 4)));
-		add(0, 5, new Bishop(Piece.Team.BLACK, grid.getLocation(0, 5)));
-		add(0, 6, new Knight(Piece.Team.BLACK, grid.getLocation(0, 6)));
-		add(0, 7, new Rook(Piece.Team.BLACK, grid.getLocation(0, 7)));
+		add(0, 0, new Rook(Piece.Team.BLACK));
+		add(0, 1, new Knight(Piece.Team.BLACK));
+		add(0, 2, new Bishop(Piece.Team.BLACK));
+		add(0, 3, new Queen(Piece.Team.BLACK));
+		add(0, 4, new King(Piece.Team.BLACK));
+		add(0, 5, new Bishop(Piece.Team.BLACK));
+		add(0, 6, new Knight(Piece.Team.BLACK));
+		add(0, 7, new Rook(Piece.Team.BLACK));
 
-		add(7, 0, new Rook(Piece.Team.WHITE, grid.getLocation(0, 0)));
-		add(7, 1, new Knight(Piece.Team.WHITE, grid.getLocation(0, 1)));
-		add(7, 2, new Bishop(Piece.Team.WHITE, grid.getLocation(0, 2)));
-		add(7, 3, new Queen(Piece.Team.WHITE, grid.getLocation(0, 3)));
-		add(7, 4, new King(Piece.Team.WHITE, grid.getLocation(0, 4)));
-		add(7, 5, new Bishop(Piece.Team.WHITE, grid.getLocation(0, 5)));
-		add(7, 6, new Knight(Piece.Team.WHITE, grid.getLocation(0, 6)));
-		add(7, 7, new Rook(Piece.Team.WHITE, grid.getLocation(0, 7)));
+		add(7, 0, new Rook(Piece.Team.WHITE));
+		add(7, 1, new Knight(Piece.Team.WHITE));
+		add(7, 2, new Bishop(Piece.Team.WHITE));
+		add(7, 3, new Queen(Piece.Team.WHITE));
+		add(7, 4, new King(Piece.Team.WHITE));
+		add(7, 5, new Bishop(Piece.Team.WHITE));
+		add(7, 6, new Knight(Piece.Team.WHITE));
+		add(7, 7, new Rook(Piece.Team.WHITE));
 
 		for (int col = 0; col < grid.cols; col++) {
-			add(1, col, new Pawn(Piece.Team.BLACK, grid.getLocation(1, col)));
+			add(1, col, new Pawn(Piece.Team.BLACK));
 		}
 
 		for (int col = 0; col < grid.cols; col++) {
-			add(6, col, new Pawn(Piece.Team.WHITE, grid.getLocation(0, col)));
+			add(6, col, new Pawn(Piece.Team.WHITE));
 		}
 	}
 	
